219c88a1bd08cadbbb3757ed96c39f874627c751
DenisM20 <denis.gmta@gmail.com>
2020-11-25 22:52:11 +0000

    added reload

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
+import ScrollReload from "./ScrollReload";
 import NavBar from "./navbar/NavBar";
 import HomePage from "./homepage/HomePage";
 import MovieDetail from "./moviedetail/MovieDetail";
@@ -24,57 +25,61 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <main style={{ marginTop: "70px" }}>
-          <Switch>
-            <Route
-              exact
-              path="/"
-              render={(routeProps) => {
-                return (
-                  <HomePage
-                    {...routeProps}
-                    searchMovie={searchMovie}
-                    setSearchMovie={setSearchMovie}
-                    searchSeries={searchSeries}
-                    setSearchSeries={setSearchSeries}
-                  />
-                );
-              }}
-            />
-            <Route
-              exact
-              path="/movies/page:num"
-              render={(routeProps) => {
-                return <MoviesList {...routeProps} searchMovie={searchMovie} />;
-              }}
-            />
-            <Route exact path="/movies/:id" component={MovieDetail} />
-            <Route
-              exact
-              path="/series/page:num"
-              render={(routeProps) => {
-                return (
-                  <SeriesList {...routeProps} searchSeries={searchSeries} />
-                );
-              }}
-            />
-            <Route exact path="/series/:id" component={SeriesDetail} />
-            <Route exact path="/toprated" component={TopRated} />
-            <Route
-              exact
-              path="/movies/toprated/page:num"
-              component={TopMovies}
-            />
-            <Route
-              exact
-              path="/series/toprated/page:num"
-              component={TopSeries}
-            />
-            <Route path="/recomendations" component={Recomendations} />
-            <Route path="/about" component={About} />
-            <Route path="/spotify">
-              <SpotifyPlayer props={"shakira"} />
-            </Route>
-          </Switch>
+          <ScrollReload>
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={(routeProps) => {
+                  return (
+                    <HomePage
+                      {...routeProps}
+                      searchMovie={searchMovie}
+                      setSearchMovie={setSearchMovie}
+                      searchSeries={searchSeries}
+                      setSearchSeries={setSearchSeries}
+                    />
+                  );
+                }}
+              />
+              <Route
+                exact
+                path="/movies/page:num"
+                render={(routeProps) => {
+                  return (
+                    <MoviesList {...routeProps} searchMovie={searchMovie} />
+                  );
+                }}
+              />
+              <Route exact path="/movies/:id" component={MovieDetail} />
+              <Route
+                exact
+                path="/series/page:num"
+                render={(routeProps) => {
+                  return (
+                    <SeriesList {...routeProps} searchSeries={searchSeries} />
+                  );
+                }}
+              />
+              <Route exact path="/series/:id" component={SeriesDetail} />
+              <Route exact path="/toprated" component={TopRated} />
+              <Route
+                exact
+                path="/movies/toprated/page:num"
+                component={TopMovies}
+              />
+              <Route
+                exact
+                path="/series/toprated/page:num"
+                component={TopSeries}
+              />
+              <Route path="/recomendations" component={Recomendations} />
+              <Route path="/about" component={About} />
+              <Route path="/spotify">
+                <SpotifyPlayer props={"shakira"} />
+              </Route>
+            </Switch>
+          </ScrollReload>
         </main>
       </BrowserRouter>
     </div>
